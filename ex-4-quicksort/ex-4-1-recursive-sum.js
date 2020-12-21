f45c1c46da1d6d1142f5cf6b7ce2cc86011d8e9d
Mike Moore <michael.moore@automattic.com>
2020-12-20 19:46:00 -0500

    add recursive sum exercise

---
+++ ex-4-quicksort/ex-4-1-recursive-sum.js
@@ -0,0 +1,12 @@
+//Exercise 4.1
+//Sum all elements in an array with a recursive function
+
+function sum( arr ) {
+    if ( arr.length <= 0) {
+        return 0;
+    }
+
+    return arr[0] + sum([...arr.slice(1, arr.length)]);
+}
+
+console.log(sum([1,2,3]));
---
+++ ex-4-quicksort/quicksort.js
@@ -0,0 +1,7 @@
+function quickSort( arr ) {
+    const pivot = arr.length - 1;
+
+    for (let i = 0; i < arr.length - 1; i++) {
+        
+    }
+}
