689fca19556ffd0e1cdc7b24d0facbbb213fa00c
Mike Moore <michael.moore@automattic.com>
2020-12-20 22:14:51 -0500

    inital quicksort.js commit

--- ex-4-quicksort/quicksort.js
+++ ex-4-quicksort/quicksort.js
@@ -1,7 +1,11 @@
 function quickSort( arr ) {
     const pivot = arr.length - 1;
+    let leftArr = [];
+    let rightArr = [];
 
     for (let i = 0; i < arr.length - 1; i++) {
-        
+        if ( arr[i] < 1) {
+            
+        }
     }
 }
