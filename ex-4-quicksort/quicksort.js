8fe9920550251ad7563513d3f7617c68a3e7de1f
Mike Moore <michael.moore@automattic.com>
2020-12-22 17:51:40 -0500

    refactor and remove un-needed conditionals

--- ex-4-quicksort/quicksort.js
+++ ex-4-quicksort/quicksort.js
@@ -1,6 +1,6 @@
 function quickSort( arr ) {
 
-    if ( arr.length < 2 ) {
+    if ( arr.length <= 1 ) {
         return arr;
     }
 
@@ -8,21 +8,15 @@ function quickSort( arr ) {
     let leftArr = [];
     let rightArr = [];
 
-    for (let i = 0; i < arr.length - 1; i++) {
+    for ( let i = 0; i < arr.length - 1; i++ ) {
         if ( arr[i] < pivot ) {
-            leftArr.push(arr[i]);
+            leftArr.push( arr[i] );
         } else {
-            rightArr.push(arr[i]);
+            rightArr.push( arr[i] );
         }
     }
-
-    if ( leftArr.length > 0 && rightArr.length > 0 ) {
-        return [...quickSort( leftArr ), pivot, ...quickSort( rightArr )];
-    } else if ( leftArr.length > 0 ) {
-        return [...quickSort(leftArr), pivot];
-    } else {
-        return [pivot, ...quickSort( rightArr )];
-    }
+    
+    return [...quickSort( leftArr ), pivot, ...quickSort( rightArr )];
 
 }
 
