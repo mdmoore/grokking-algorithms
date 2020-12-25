18be44cf2ec065f1a25143b730bdda909b51f85b
Mike Moore <michael.moore@automattic.com>
2020-12-24 21:53:30 -0500

    return correctly

--- ex-3-recursion/look-for-key.js
+++ ex-3-recursion/look-for-key.js
@@ -1,22 +1,18 @@
 function lookForKey( arr ){
-    let result = "not found";
-
     for ( let i = 0; i < arr.length; i++ ) {
         if( arr[i] == "key" ){
-            console.log( "Key found.");
-            result = "Key found";
-        } else if( Array.isArray( arr[i] ) ){
-            result = lookForKey( arr[i] );
-        }
+            return true;
+        } else if( Array.isArray( arr[i] ) && lookForKey( arr[i] ) ) {
+            return true;
+        } 
     }
 
-    return result;
+    return false;
 }
 
 let arr = [
-    ["stuff", "things"],
-    ["stuff", "things"],
-    ["stuff", "things", "key"]
-];
+    ["foo"],
+    ["key"]
+  ];
 
 console.log( lookForKey( arr ) );
